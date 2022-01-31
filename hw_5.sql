5c30550ce75102343d820b74e647b1db1e487b04
Sergey Yapaev <sergeyyapaev@yandex.ru>
2022-01-31 23:24:18 +0300

    Fixes homework 5

--- hw_5.sql
+++ hw_5.sql
@@ -23,38 +23,34 @@ GROUP BY a."name";
 
 -- Задание 4
 SELECT m."name"
-FROM musician_album ma 
-JOIN musicians m ON ma.musician_id = m.id
-JOIN albums a ON ma.album_id = a.id
-GROUP BY m."name"
-HAVING every(a.release_year != 2020);
+FROM musicians m 
+WHERE m.id NOT IN (
+    SELECT DISTINCT ma.musician_id 
+    FROM musician_album ma 
+    JOIN albums a ON ma.album_id = a.id 
+    WHERE a.release_year = 2020
+);
 
 
 -- Задание 5
-WITH musicians_info AS (
-    SELECT m.id AS musician_id, m."name", ma.album_id
-    FROM musician_album ma
-    JOIN musicians m ON ma.musician_id = m.id
-)
-SELECT c."name"
-FROM track_collection tc 
+SELECT DISTINCT c."name"
+FROM musician_album ma 
+JOIN musicians m ON ma.musician_id = m.id 
+JOIN albums a ON ma.album_id = a.id 
+JOIN tracks t ON a.id = t.album_id 
+JOIN track_collection tc ON tc.track_id = t.id 
 JOIN collections c ON tc.collection_id = c.id 
-JOIN tracks t ON tc.track_id = t.id 
-JOIN musicians_info mi ON t.album_id = mi.album_id
-GROUP BY c."name"
-HAVING bool_or(mi."name" = 'Imagine Dragons');
+WHERE m."name" = 'Imagine Dragons';
 
 
 -- Задание 6
-SELECT a."name" AS album
+SELECT a."name"
 FROM musician_album ma 
-JOIN albums a ON ma.album_id = a.id
-WHERE ma.musician_id IN (
-    SELECT mg.musician_id
-    FROM musician_genre mg
-    GROUP BY mg.musician_id 
-    HAVING count(mg.genre_id) > 1
-);
+JOIN musicians m ON ma.musician_id = m.id 
+JOIN albums a ON ma.album_id = a.id 
+JOIN musician_genre mg ON m.id = mg.musician_id
+GROUP BY mg.musician_id, a."name" 
+HAVING count(mg.genre_id) > 1;
     
 
 -- Задание 7
@@ -66,18 +62,12 @@ ORDER BY t."name";
 
 
 -- Задание 8
-SELECT m."name" AS musician
+SELECT m."name"
 FROM musician_album ma 
-JOIN musicians m ON m.id = ma.musician_id 
-JOIN (
-    SELECT t.album_id, min(t.duration_sec) AS dur
-    FROM tracks t
-    GROUP BY t.album_id
-) AS "temp" USING (album_id)
-WHERE "temp".dur = (
-    SELECT min(duration_sec)
-    FROM tracks
-);
+JOIN musicians m ON ma.musician_id = m.id 
+JOIN albums a ON ma.album_id = a.id 
+JOIN tracks t ON a.id = t.album_id
+WHERE t.duration_sec = (SELECT min(t.duration_sec) FROM tracks t);
 
 
 -- Задание 9
